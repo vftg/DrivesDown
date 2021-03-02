d19c5a45b46ea7d8705716543513c8ed4be76296
vftg <76833464+vftg@users.noreply.github.com>
2021-03-03 01:21:06 +0800

    Update upfiles.sh

--- upfiles.sh
+++ upfiles.sh
@@ -4,6 +4,6 @@ aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=
 #for ((i=1, j=20; i<=341; i=i+20, j=j+20))
 #do
 #aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 --select-file=$i-$j *.torrent
-rclone move -v --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ od:/test/
+rclone move -v --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" $GITHUB_WORKSPACE/download od:/test/
 #echo "$i-$j 上传完成"
 #done
