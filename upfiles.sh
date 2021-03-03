b8df42ee517ed4b04a5f4f8025476fb3b019ca9d
y2v <76833464+y2v@users.noreply.github.com>
2021-03-03 22:22:58 +0800

    Update upfiles.sh

--- upfiles.sh
+++ upfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://dl.dmhy.org/2017/03/01/58cba62b422cf30de75dd3883cccc57fc1bebebf.torrent
+wget https://dl.dmhy.org/2016/06/06/ef6b05231c441553e8704d7b151c98828dff7953.torrent
 aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 #for ((i=1, j=20; i<=341; i=i+20, j=j+20))
 #do
