13d21071ce3b6db1cdf2b5bd948bde7e26c51586
y2v <76833464+y2v@users.noreply.github.com>
2021-03-12 00:58:58 +0800

    Update upfiles.sh

--- upfiles.sh
+++ upfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://pan.mduu.top/dltorrent/Tenet.2020.IMAX.1080p.BluRay.x264-FLAME-[rarbg.to]
+wget https://pan.mduu.top/dltorrent/Tenet.2020.IMAX.1080p.BluRay.x264-FLAME-[rarbg.to].torrent
 aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 #for ((i=1, j=20; i<=341; i=i+20, j=j+20))
 #do
