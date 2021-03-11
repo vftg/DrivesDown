36f6faa20f4e0e8c4621adca499008797fc84d05
y2v <76833464+y2v@users.noreply.github.com>
2021-03-12 00:57:33 +0800

    Update upfiles.sh

--- upfiles.sh
+++ upfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://www.rarbgtor.org/download.php?id=7g9xfpu&h=f75&f=Raya.and.the.Last.Dragon.2021.2160p.DSNP.WEB-DL.x265.10bit.HDR.DDP5.1-NOGRP-[rarbg.to].torrent
+wget https://pan.mduu.top/dltorrent/Tenet.2020.IMAX.1080p.BluRay.x264-FLAME-[rarbg.to]
 aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 #for ((i=1, j=20; i<=341; i=i+20, j=j+20))
 #do
