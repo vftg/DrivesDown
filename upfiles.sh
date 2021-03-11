a7e70aefec3d226bad84eb5a9cfa11d3c370c0e8
y2v <76833464+y2v@users.noreply.github.com>
2021-03-11 22:38:10 +0800

    Update upfiles.sh

--- upfiles.sh
+++ upfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget http://42.193.180.59/aa/a.torrent
+wget https://pan.mduu.top/dltorrent/Tom.and.Jerry.2021.2160p.HMAX.WEB-DL.x265.10bit.HDR.DDP5.1.Atmos-SWTYBLZ.torrent
 aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 #for ((i=1, j=20; i<=341; i=i+20, j=j+20))
 #do
